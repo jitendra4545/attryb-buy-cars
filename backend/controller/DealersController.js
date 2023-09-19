eaee3712f4f304ece4e1d91d71f8fc4f4ebbc669
jitendra4545 <jitendra45.js45@gmail.com>
2023-09-19 21:59:10 +0530

    filter sorting working fine

--- backend/controller/DealersController.js
+++ backend/controller/DealersController.js
@@ -24,9 +24,6 @@ const upload = multer({ storage });
 
 
 
-// DealerRouter.get("/", (req, res) => {
-//     res.send('hi this Dealers')
-// })
 
 
 DealerRouter.post("/add", upload.single('image'), Authentication, async (req, res) => {
@@ -57,11 +54,60 @@ DealerRouter.post("/add", upload.single('image'), Authentication, async (req, re
 
 
 DealerRouter.get("/", async (req, res) => {
+    const { order, filter, search } = req.query;
     try {
-        let AllData = await DealearsModel.find({}).populate("oem_id");
-        res.send(AllData)
-    } catch (err) {
-        res.send({ "msg": "Something went wrong ! Unable to get Cars", "err": err.message })
+      if (filter === "price") {
+        let data;
+        //if order is desc then we are sorting the data and sending on fronted on the basis of price in descending order
+  
+        if (order == "desc") {
+          data = await DealearsModel.find()
+            .populate("oem_id")
+            .sort({ price: -1 });
+          console.log(data);
+        } else {
+          //else if  then we are sorting the data and sending on fronted on the basis of price in ascending price
+          data = await DealearsModel.find()
+            .populate("oem_id")
+            .sort({ price: 1 });
+        }
+  
+        res.send(data);
+      } else if (filter == "mileage") {
+        let data = await DealearsModel.find().populate("oem_id").lean();
+  
+        if (order == "desc") {
+          //if data order is descegin and filter is mileage than we are populated the oem model so that
+          //we can get the desired mileage data from the realation oemId and sort on the basis of the data
+          //this will lean the data in plain js object and after sorting we can send to frontend
+  
+          data.sort((a, b) => b.oem_id.mileage - a.oem_id.mileage);
+        } else {
+          data.sort((a, b) => a.oem_id.mileage - b.oem_id.mileage);
+        }
+  
+        res.send( data );
+      } else if (filter === "colors") {
+        //this is used for colors filter as we have populated oemId and mathing the colors with regex query
+        //having opitons i which enable case sensitive searching
+  
+        let data = await DealearsModel.find().populate({
+          path: "oem_id",
+          match: { color: { $regex: order, $options: "i" } },
+        });
+        //math will return the documnets which are  not found allso but wich null so we are filtering and sending on frontend
+        data = data.filter((el) => el.oem_id !== null);
+  
+        res.send(data);
+      } else {
+        let data = await DealearsModel.find().populate({
+          path: "oem_id",
+        });
+  
+        res.send( data );
+      }
+    } catch (error) {
+      res.send({ "msg":"Something Went Wrong !","err":err.message});
     }
 })
 
@@ -96,7 +142,16 @@ DealerRouter.delete("/:id",async(req,res)=>{
 })
 
 
-
+DealerRouter.get("/:id",async(req,res)=>{
+    const id=req.params.id
+    console.log(id)
+    try {
+        let AllData = await DealearsModel.find({_id:id}).populate("oem_id");
+        res.send(AllData)
+    } catch (err) {
+        res.send({ "msg": "Something went wrong ! Unable to get Cars", "err": err.message })
+    }
+})
 
 
 
